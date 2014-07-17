9f460406ae2ea7f82d76ad59e57b76e3ba883e74
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2014-07-17 15:17:23 -0300

    alterado o plugin do ckeditor.

--- assets/js/ckeditor/config.js
+++ assets/js/ckeditor/config.js
@@ -1,10 +1,18 @@
-/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	config.language = 'pt-br';
-	// config.uiColor = '#AADC6E';
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.editorConfig = function(config) {
+    var url = '/admin/assets/js/ckeditor/ckfinder/';
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.language = 'pt-br';
+    config.skin = 'BootstrapCK-Skin';
+    config.filebrowserBrowseUrl = url + 'ckfinder.html';
+    config.filebrowserImageBrowseUrl = url + 'ckfinder.html?Type=Images';
+    config.filebrowserFlashBrowseUrl = url + 'ckfinder.html?Type=Flash';
+    config.filebrowserUploadUrl = url + 'core/connector/php/connector.php?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = url + 'core/connector/php/connector.php?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl = url + 'core/connector/php/connector.php?command=QuickUpload&type=Flash';
 };
