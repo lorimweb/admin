c356967b847ef1703a92e50bba0945708f9dc943
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2014-11-26 00:26:50 -0200

    alterada a forma de como é salva a sessão.

--- application/migrations/001_cria_estrutura_inicial.php
+++ application/migrations/001_cria_estrutura_inicial.php
@@ -74,6 +74,8 @@ class Migration_cria_estrutura_inicial extends CI_Migration {
 	 */
 	private function _cria_tabelas()
 	{
+		$sql = 'CREATE TABLE IF NOT EXISTS  `ci_sessions` (  session_id varchar(40) DEFAULT "0" NOT NULL,  ip_address varchar(45) DEFAULT "0" NOT NULL,  user_agent varchar(120) NOT NULL,  last_activity int(10) unsigned DEFAULT 0 NOT NULL,  user_data text NOT NULL,  PRIMARY KEY (session_id),  KEY `last_activity_idx` (`last_activity`) )';
+		$this->db->query($sql);
 		foreach ($this->_modulos as $modulo)
 		{
 			$this->{$modulo.'_model'}->adicionar_tabela();
